b1fdc763d6662c8c4a122b64abcbd88051023698
Satimru <116436390+Satimru@users.noreply.github.com>
2022-11-24 11:28:26 +0300

    Add files via upload

---
+++ PetroGeoSim/tests/test_models.py
@@ -0,0 +1,27 @@
+import unittest
+from PetroGeoSim.models import Model
+from PetroGeoSim.regions import Region
+
+
+class TestModel(unittest.TestCase):
+    # setUp method is overridden from the parent class TestCase
+    def setUp(self):
+        self.model = Model('lolol')
+
+    # Each test method starts with the keyword test_
+    def test_serialize(self):
+        a = tuple('A')
+        b = self.model.serialize(a)
+        self.assertEquals(type(dict()), b)
+    def test_add_regions(self):
+      """В PetroGeoSim Probabillistic Reserves RU сказано, что нельзя добавлять
+      два региона с одинаковыми именами. В данной функции я создал два региона
+      с одинаковыми именами и разными составами. При этом никакой ошибки не
+      возникает.
+      """
+
+      region_1 = Region('A','Sandstone')
+      region_2 = Region('A','Dolomite')
+      print(region_1)
+      print(region_2)
+      self.assertTrue(region_1 == region_2)
